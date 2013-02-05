182ddf8c3c1289a0770226b09d284b44746cf023
GeoffreyBassett <street.email@gmail.com>
2013-02-05 12:22:12 -0500

    Button added

--- main.lua
+++ main.lua
@@ -5,5 +5,19 @@ require "CiderDebugger";--------------------------------------------------------
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+local widget = require "widget"
 
+local button = widget.newButton{
+    label = "The Button",
+    font = "HelveticaNeue-Bold",
+    fontSize = 16
+}
+button.x = display.contentCenterX
+button.y = display.contentCenterY
+
+local onClick = function (event )
+        if event.phase == "release" then
+            
+        end
+    end
 
